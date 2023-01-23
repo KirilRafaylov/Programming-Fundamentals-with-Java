dca23a1bd802828b60cb0fa5e03efd72b1c5db41
KirilRaf <100319602+KirilRaf@users.noreply.github.com>
2023-01-23 19:44:17 +0200

    Adding new folder called "Objects and Classes"

---
+++ Objects and Classes/src/Lab/Demo.java
@@ -0,0 +1,13 @@
+package Lab;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Scanner;
+
+public class Demo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        LocalDateTime today = LocalDateTime.now ();
+        
+    }
+}
